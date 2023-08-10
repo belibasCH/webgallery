3b4fc98401779780259eabd937cd7f4654a2f341
unknown <elias.braem@students.fhnw.ch>
2023-08-10 23:22:13 +0200

    add css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Form from './Form';
 import Image from './Image';
 import { storage } from './firebase';
+import './App.css';
 import { getStorage, ref, list, getDownloadURL } from "firebase/storage";
 
 function App() {
