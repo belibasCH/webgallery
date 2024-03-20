5b09e617d3ee6d8a5bbaa852f7ab15f224f1f282
Elias Braem <elias.braem@software-brauerei.ch>
2024-03-20 22:47:47 +0100

    basic style

--- src/pages/Slideshow.js
+++ src/pages/Slideshow.js
@@ -1,27 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Image from '../components/Image';
-import { storage } from '../config/firebase';
+import { db } from '../config/firebase';
 import '../css/App.css';
-import { getStorage, ref, list, getDownloadURL } from "firebase/storage";
+import { getDocs, collection } from "firebase/firestore";
 
 function Slideshow() {
   const defaultimage = { _location: { path: "js/1" } }
   const [imageList, setImageList] = useState([]);
   const [currentimage, setCurrentImage] = useState(0);
 
-  useEffect(() => {
-    const storageRef = ref(storage, 'js/');
-    list(storageRef).then((res) => {
-      setImageList(res.items);
-    })
-    const interval = setInterval(() => {
-      const storageRef = ref(storage, 'js/');
-      list(storageRef).then((res) => {
-        setImageList(res.items);
-      })
-    }, 600000);
-    return () => clearInterval(interval);
-
+  useEffect(() =>{
+    async function fetchData() {
+      const newImages = [];
+      const querySnapshot = await getDocs(collection(db, "js"));
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data().description}`);
+        newImages.push(doc.data());
+      });
+      setImageList(newImages);
+    }
+    fetchData();
   }, [])
 
   useEffect(() => {
@@ -34,8 +32,11 @@ function Slideshow() {
   return (
     <div className="image-list">
       {
-        imageList.map((img, index) => (
-          <Image key={index} src={img} active={currentimage == index ? true : false} />
+        imageList.map((element, index) => (
+          <div className={['imageContainer', index === currentimage ? 'active' : 'disable'].join(' ')} key={index} >
+          <img key={index} src={element.url} />
+          <h2 className='description'>{element.description}</h2>
+          </div>
         ))
       }
     </div>
